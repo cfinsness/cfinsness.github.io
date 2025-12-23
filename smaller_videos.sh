4ea44607ea8bbb3a70214140f62d4480070c2c57
Ilya Kavalerov <ilyakavalerov@gmail.com>
2025-12-23 13:51:42 -0800

    Add smaller videos script

--- smaller_videos.sh
+++ smaller_videos.sh
@@ -1,7 +1,7 @@
 
 for f in *; do ffmpeg -i ${f} -c:v libx264 -crf 23 -profile:v high -level 4.1 -pix_fmt yuv420p -r 30 -an -movflags +faststart for-web/${f}; done
 
-# ffmpeg -i ${f} -c:v libx264 -crf 23 -profile:v high -level 4.1 -pix_fmt yuv420p -r 30 -an -movflags +faststart for-web/${f}
+ffmpeg -i ${f} -c:v libx264 -crf 23 -profile:v high -level 4.1 -pix_fmt yuv420p -r 30 -an -movflags +faststart small-${f}
 
 
 
